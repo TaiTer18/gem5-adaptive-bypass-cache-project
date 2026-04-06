002fd526a1ae61bf37cea8cfa8098dc53380c284
Nicholas Tait <nicholastait15@gmail.com>
2026-04-06 16:21:12 -0700

    updated synthetic benchmark

--- slurm_evaluation/synthetic_cache_killer.cpp
+++ slurm_evaluation/synthetic_cache_killer.cpp
@@ -2,22 +2,31 @@
 #include <vector>
 
 int main() {
-    int matrix_size = 1048576; // 4MB matrix
-    int vector_size = 32768;   // 128kB vector
-
+    int matrix_size = 1048576; // 4MB matrix, easily flushes 1MB LRU
+    int vector_size = 32768;   // 128kB vector, fits nicely in L2
+    
     std::vector<int> streaming_matrix(matrix_size, 1);
     std::vector<int> hot_vector(vector_size, 2);
-
+    
     long long sum = 0;
+    
+    for (int iter = 0; iter < 20; iter++) {
+        
 
-    for (int iter = 0; iter < 10; iter++) {
         for (int i = 0; i < matrix_size; i++) {
+            streaming_matrix[i] ^= (i % 256);
             sum += streaming_matrix[i]; 
         }
-        for (int j = 0; j < 5; j++) {
-            for (int k = 0; k < vector_size; k++) {
-                sum += hot_vector[k]; 
+
+        for (int k = 0; k < vector_size; k++) {
+            int val = hot_vector[k];
+            
+            for (int w = 0; w < 50; w++) {
+                val = (val ^ (w * 7)) + 1;
             }
+            
+            hot_vector[k] = val;
+            sum += val;
         }
     }
 
